8298c5a70874ab27cba98f31f87570cd75af0bba
SophiaNeu <sonya.neudakhina@gmail.com>
2016-06-20 20:48:30 +0300

    test

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,8 +5,8 @@ from calc import calc
 
 @pytest.mark.parametrize('expression, expected', [
     ('2+2', 4),
-    # ('2+4', 6),
+    ('2+4', 6),
 ])
 def test_addition(expression, expected):
-    result = calc.calc(expression)
+    result = calc(expression)
     assert result == expected
