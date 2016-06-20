fe2674efcb654dd804a988a6ea476ebe11956018
SophiaNeu <sonya.neudakhina@gmail.com>
2016-06-20 20:48:11 +0300

    modify calc function

--- calc.py
+++ calc.py
@@ -1,2 +1,9 @@
 def calc(expression):
-    return 4
+    try:
+        res = expression.split('+')
+        result = []
+        for i in range(len(res)):
+            result.append(int(res[i]))
+        return sum(result)
+    except:
+        pass
