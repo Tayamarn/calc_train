bf47fcb118c0cf48603f8a1d6841865d3ce852e6
SophiaNeu <sonya.neudakhina@gmail.com>
2016-06-21 18:30:38 +0300

    add tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,7 +5,11 @@ from calc import calc
 
 @pytest.mark.parametrize('expression, expected', [
     ('2+2', 4),
-    ('2+4', 6),
+    ('2+4.5', 6.5),
+    ('11', 11),
+    ('', None),
+    ('+15', None),
+    ('fgjkfdljgk', None),
 ])
 def test_addition(expression, expected):
     result = calc(expression)
